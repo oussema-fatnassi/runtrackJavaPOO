3ec027bd13055249bc4b543bef4e63f5f6fe0a18
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-02 10:10:34 +0200

    Static variable access error

---
+++ Jour07/Job1/Toto.java
@@ -0,0 +1,15 @@
+package Jour07.Job1;
+
+class Toto {
+    int toto = 0;
+    Toto(){
+        toto = toto + 1;
+    }
+    public static void main(String[] tutu) {
+        Toto t1 = new Toto();
+        Toto t2 = new Toto();
+        //System.out.println("Toto: " + toto);            // Error: non-static variable toto cannot be referenced from a static context
+        //We should use t1.toto instead of toto or make toto static
+        System.out.println("Toto: " + t1.toto);
+    }
+}
