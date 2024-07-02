73e36eebc4367236a5b4f4771e8557f1a40b8fb6
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-07-02 10:55:59 +0200

    Create Rectangle class

---
+++ Jour07/Job3/GeometricFigures.java
@@ -0,0 +1,9 @@
+package Jour07.Job3;
+
+public class GeometricFigures {
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(5, 10);
+        System.out.println("The area of the rectangle is: " + ((Rectangle) r).area());        
+    }
+}
